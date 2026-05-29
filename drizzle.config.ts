9b9251f5730bfa018bc5fe5bdebc938c686fbf28
muhammad-fahmi <fahmipradana4@gmail.com>
2026-05-29 13:33:25 +0700

    fix(db): change dialect to turso in drizzle config

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "drizzle-kit";
 export default defineConfig({
   schema: "./db/schema.ts",
   out: "./drizzle",
-  dialect: "sqlite",
+  dialect: "turso",
   dbCredentials: {
     url: process.env.DATABASE_URL || "sqlite.db",
     token: process.env.DATABASE_AUTH_TOKEN,
